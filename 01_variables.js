0fb48dc3fc64eb0a04ab2c9caf08b1de27ba8a72
Gunjan258 <gunjankumarsingh258@gmail.com>
2026-04-24 02:31:32 +0530

    first commit

---
+++ 01_variables.js
@@ -0,0 +1,12 @@
+const accountid =144553
+let accountemail = "Gunjan@gmail.com"
+var accountpassword = "12345"
+
+accountcity = "Jaipur"
+let accountstate;
+
+accountemail = "google@gmail.com"
+accountpassword = "121212"
+accountcity = "Banglore"
+
+console.log([accountemail,accountpassword,accountid,accountcity])
